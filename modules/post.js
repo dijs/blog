fa7b67ebfc935099467b945a484f00e179043450
Richard van der Dys <richard.vanderdys@gmail.com>
2016-01-23 17:37:05 -0600

    Added icon and fixed post and styles

--- modules/post.js
+++ modules/post.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 export default function(props) {
-  const {title, contents} = props.file
+  const {title, date, contents} = props.file
   const html = {__html: contents.toString()}
   return <html>
     <head>
       <link rel='stylesheet' type='text/css' href='/css/post.css' />
       <link rel='stylesheet' type='text/css' href='//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/styles/default.min.css' />
+      <link rel="icon" type="image/png" href="/public/favicon.png" />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <title>{title}</title>
     </head>
@@ -15,6 +16,7 @@ export default function(props) {
         <a href='/' className='back'>&laquo; Back</a>
         <h1>{title}</h1>
       </header>
+      <time>{date}</time>
       <article dangerouslySetInnerHTML={html} />
     </body>
   </html>
